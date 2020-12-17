a6841e28a2af6ae9dd6ff34000f3877aba3e2d8d
Phuffick <57110647+Phuffick@users.noreply.github.com>
2020-12-17 20:38:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,23 +3,16 @@
 
 
 int main() {
-
 	std::string Name;
-
 	std::cout << "Input name of the file: ";
-
 	getline(std::cin, Name, '\n');
 
 	if (!empty(Name)) {
-
 		std::ifstream fin(Name);
-
 		if (fin.is_open()) {
-
 			fin.close();
-
+			
 			Array <int> arr(Name);
-
 			arr.PrintArray();
 			arr.ChoiceSort();
 			arr.PrintArray();
@@ -28,23 +21,15 @@ int main() {
 			arr.InsertSort();
 			arr.PrintArray();
 			arr.BinaryFinnd(38);
-			std::cout << std::endl;
-		}
-
+			std::cout <<'\n';
+		} 
 		else {
-
 			fin.close();
-
-			std::cout << "There is not that file! " << std::endl;
-
+			std::cout << "There is not that file! " << '\n';
 		}
-
 	}
-
 	else {
-
 		Array <int> arr(10);
-
 		arr.PrintArray();
 		arr.ChoiceSort();
 		arr.PrintArray();
@@ -54,8 +39,7 @@ int main() {
 		arr.PrintArray();
 		arr.BinaryFinnd(24);
 
-		std::cout << std::endl;
-
+		std::cout << '\n';
 	}
 
 	system("pause");
